37e2a12183648b51c705a9d03e5921f7be51d8de
Ruhama Yared <ruhamayared1@gmail.com>
2023-06-06 12:08:55 -0400

    Ran migrations and updated bit.io tables.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 
+// Knex.js is a SQL query builder. It is used to build SQL queries in JavaScript.
 const knex = require("knex")
 const knexConfig = require("./knexfile")
 
@@ -13,26 +14,27 @@ const PORT = process.env.PORT || 3000
 // Middleware
 app.use(express.json())
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
-
 // Test the database connection
-pool.connect((err, client, done) => {
-  if (err) throw err
-  console.log("Connected to the database")
-  done()
-})
-
-// Add your routes and API endpoints below this section
+db.raw("SELECT 1")
+  .then(() => {
+    console.log("Connected to the database.")
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err)
+  })
 
 // Test route to see if backend is working correctly
 app.get("/tests", async (req, res) => {
   try {
-    const tests = await getTests()
+    const tests = await db.select().from("tests")
     res.json(tests)
   } catch (err) {
+    console.error("Error retrieving tests:", err)
     res.status(500).json({ message: "Internal server error" })
   }
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
 })
